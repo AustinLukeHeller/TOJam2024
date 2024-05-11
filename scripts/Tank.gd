f2e828d0923c719aaad15adaa4a21172287ec0e2
JohnLeeErg <32018424+JohnLeeErg@users.noreply.github.com>
2024-05-11 14:01:31 -0400

    floor

---
+++ scripts/Tank.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@export var maxAngle: float
+@export var turnAccel: float
+@export var turnMaxSpeed: float
+
+var velocity: float = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_pressed("Left")):
+		velocity+=turnAccel*delta
+	elif(Input.is_action_pressed("Right")):
+		velocity-=turnAccel*delta	
+	else:
+		velocity = move_toward(velocity,0,turnAccel)
+	velocity = clamp(velocity,-turnMaxSpeed,turnMaxSpeed)
+	
+	rotation_degrees.y=clamp(rotation_degrees.y+velocity*delta,-maxAngle,maxAngle)
+	if(abs(rotation_degrees.y)==maxAngle):
+		velocity=0
+	pass

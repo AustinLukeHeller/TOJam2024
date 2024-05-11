9471dd64e9f4dc1e877aed789e0db0b425d7a174
Silver Lewis <silver.lewis@kongsberggeospatial.com>
2024-05-11 12:36:23 -0400

    pushing finished flippers and input settings

---
+++ scenes/PinballObjects/flipper.gd
@@ -0,0 +1,24 @@
+extends AnimatableBody3D
+
+@export var rotation_direction := 1.0  # Use 1.0 for clockwise and -1.0 for counter-clockwise
+@export var flipper_rotation_speed := 2.5
+@export var flipper_max_rotation := 45.0  # Set max rotation always positive
+@export var flipper_min_rotation := 0.0  # Min rotation should always be less than max rotation
+@export var tiltedTable := false;
+@export var muted := false;
+@onready var flip_sound = $Flip_Sound
+@onready var return_sound = $Return_Sound
+
+
+
+func _physics_process(delta):
+	if tiltedTable:
+		return
+	if (rotation_direction < 0 and Input.is_action_pressed("Right")) or (rotation_direction > 0 and Input.is_action_pressed("Left")):
+		rotation_degrees.y = lerp(rotation_degrees.y, flipper_max_rotation * rotation_direction, flipper_rotation_speed * delta)
+	else:
+		rotation_degrees.y = lerp(rotation_degrees.y, flipper_min_rotation * rotation_direction, flipper_rotation_speed * delta)
+	if !muted and (Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right")):
+		flip_sound.play()
+	if !muted and (Input.is_action_just_released("Left") or Input.is_action_just_released("Right")):
+		return_sound.play()

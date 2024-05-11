8e246468a95a713b481b6fb7cf5a494a12b86955
JohnLeeErg <32018424+JohnLeeErg@users.noreply.github.com>
2024-05-11 18:57:29 -0400

    scores n such

--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -3,26 +3,30 @@ extends Node3D
 @export var maxAngle: float
 @export var turnAccel: float
 @export var turnMaxSpeed: float
-
+var activated: bool = false
 var velocity: float = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect("scoreEffect1",_turnOnMoving)
 	pass # Replace with function body.
 
-
+func _turnOnMoving():
+	activated=true
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_key_pressed(KEY_2)):
 		position+=Vector3.RIGHT*delta
-	if(Input.is_action_pressed("Left")):
-		velocity+=turnAccel*delta
-	elif(Input.is_action_pressed("Right")):
-		velocity-=turnAccel*delta
-	else:
-		velocity = move_toward(velocity,0,turnAccel)
-	velocity = clamp(velocity,-turnMaxSpeed,turnMaxSpeed)
-	
-	rotation_degrees.y=clamp(rotation_degrees.y+velocity*delta,-maxAngle,maxAngle)
-	if(abs(rotation_degrees.y)==maxAngle):
-		velocity=0
+	if(activated):
+		if(Input.is_action_pressed("Left")):
+			velocity+=turnAccel*delta
+		elif(Input.is_action_pressed("Right")):
+			velocity-=turnAccel*delta
+		else:
+			velocity = move_toward(velocity,0,turnAccel)
+		velocity = clamp(velocity,-turnMaxSpeed,turnMaxSpeed)
+		
+		rotation_degrees.y=clamp(rotation_degrees.y+velocity*delta,-maxAngle,maxAngle)
+		if(abs(rotation_degrees.y)==maxAngle):
+			velocity=0
 	pass
--- scripts/scoreAndProgression.gd
+++ scripts/scoreAndProgression.gd
@@ -2,13 +2,27 @@ extends Node
 
 @export var score: float = 0.0
 @export var scoreGoals:Array[float]
-@export var scoreActions: Array[Signal]
 var currentGoal: int = 0
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _enter_tree():
+	for i in range(0,scoreGoals.size()):
+		add_user_signal("scoreEffect"+str(i),[])
 	pass # Replace with function body.
 
+func _gainPoints(points: float):
+	score+=points
+	#update ui
+	_checkGoals()
 
+func _checkGoals():
+	if(score>=scoreGoals[currentGoal]):
+		emit_signal("scoreEffect"+str(currentGoal))
+		if(currentGoal<scoreGoals.size()-1):
+			currentGoal+=1
+		#else switch into infinite goals mode
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(Input.is_key_pressed(KEY_6)):
+		_gainPoints(600)
 	pass

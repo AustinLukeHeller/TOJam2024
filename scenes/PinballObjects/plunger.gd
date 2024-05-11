e606cd0762e721793c140901721df29eb67a7f9b
Silver Lewis <silver.lewis@kongsberggeospatial.com>
2024-05-11 12:50:49 -0400

    added plunger

--- scenes/PinballObjects/plunger.gd
+++ scenes/PinballObjects/plunger.gd
@@ -1,6 +1,8 @@
 extends AnimatableBody3D
 @onready var sound = $Sound
 
+@export var tiltedTable = false
+@export var muted := false;
 @export var push_speed := 10.0
 @export var pull_speed := 2.0
 @export var max_pullback := 2.0
@@ -13,20 +15,18 @@ var start_position: Vector3
 func _ready():
 	start_position = position
 
-
 func _physics_process(delta):
-	if Global.tilt:
+	if tiltedTable:
 		return
-	if Input.is_action_pressed("Plunger") and current_pullback < max_pullback:
+	if Input.is_action_pressed("Launch") and current_pullback < max_pullback:
 		current_pullback += pull_speed * delta
 		return_speed = push_speed * (current_pullback / max_pullback)
-	elif not Input.is_action_pressed("Plunger") and current_pullback > 0:
+	elif not Input.is_action_pressed("Launch") and current_pullback > 0:
 		current_pullback -= return_speed * delta
 	
-	if Input.is_action_just_released("Plunger"):
-		if !Global.mute:
+	if Input.is_action_just_released("Launch"):
+		if !muted:
 			sound.play()
-		#Print.debug("Plunger Released, Speed %s" % [return_speed])
 	
 	if current_pullback < 0:
 		current_pullback = 0
